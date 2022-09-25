683c78374d26b96c6a3f911c617acf517ef59c78
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-09-25 17:57:02 -0400

    saving progress (first with a dep)

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,79 @@
 
 #![forbid(unsafe_code)]
 #![allow(dead_code)] // For now...
+#![allow(unused_variables)] // For now...
+
+use rand::{Rng, distributions::Bernoulli};
 
 fn main() {
     println!("Hello world!");
 }
 
 mod executor {
-    use crate::data;
-
-    pub trait CRPTarget<SymT: data::StateSym> {
-        type CoT: data::StateCo;
-        fn top(&self) -> data::BlockId;
-        fn exec(&self, state: Self::CoT, block: data::BlockId)
-            -> data::FullCBS<Self::CoT, SymT>;
-    }
-
-    fn execute_cbs<SymT: data::StateSym, T: CRPTarget<SymT>>(
-        target: T,
-        cbs: data::FullCBS<T::CoT, SymT>
-    ) -> data::CBSSet<T::CoT, SymT> {
-        todo!()
-    }
-}
-
-mod data {
     use std::collections::{HashMap, HashSet};
 
-    pub trait StateCo {
+    trait CRPTarget<SymT: StateSym> {
+        type CoT: StateCo;
+        fn top(&self) -> BlockId;
+        fn exec(&self, state: Self::CoT, block: BlockId, pathsrc: &mut Pathsrc)
+            -> FullCBS<Self::CoT, SymT>;
+    }
+
+    fn execute_cbs<SymT: StateSym, T: CRPTarget<SymT>>(
+        target: T,
+        cbs: FullCBS<T::CoT, SymT>
+    ) -> CBSTree<T::CoT, SymT> {
+        let FullCBS {
+           state_c, state_s, block 
+        } = cbs;
+        let mut pathsrc = Pathsrc::default();
+        let initial_exec = target.exec(state_c, block, &mut pathsrc);
+        todo!()
+    }
+
+    #[derive(Clone, Default)]
+    struct Pathsrc {
+        known_part: Vec<bool>,
+        found_part: Vec<bool>,
+        idx: usize,
+    }
+
+    impl Pathsrc {
+        fn from_known(known_part: Vec<bool>) -> Self {
+            let at_known = !known_part.is_empty();
+            Self {
+                known_part,
+                found_part: Vec::new(),
+                ..Default::default()
+            }
+        }
+
+        fn solidify(&mut self) {
+            self.known_part.append(&mut self.found_part);
+        }
+
+        fn get_choice<'a>(&mut self, &'a impl rng::Rng) -> bool {
+            let known_len = self.known_part.len();
+            if self.idx >= known_len {
+                self.known_part[self.idx]
+            } else if self.idx >= known_len + self.found_part.len() {
+                self.found_part[self.idx + known_len]
+            } else {
+                let choice = 
+            }
+        }
+    }
+
+    trait StateCo {
         // Methods will be defined here...
     }
 
-    pub trait StateSym {
+    trait StateSym {
         // Methods will be defined here...
     }
 
-    pub struct Conj<SymT: StateSym>(Vec<SymT>);
-    pub struct Disj<SymT: StateSym>(Vec<SymT>);
+    struct Conj<SymT: StateSym>(Vec<SymT>);
+    struct Disj<SymT: StateSym>(Vec<SymT>);
 
     impl<SymT: StateSym> StateSym for Conj<SymT> {
         // Implementation will be defined here...
@@ -48,34 +85,34 @@ mod data {
     }
 
     #[derive(Copy, Clone, PartialEq, Hash)]
-    pub struct BlockId(u32);
+    struct BlockId(u32);
 
-    pub struct FullCBS<CoT: StateCo, SymT: StateSym> {
+    struct FullCBS<CoT: StateCo, SymT: StateSym> {
         state_c: CoT,
         state_s: Conj<SymT>,
         block: BlockId,
     }
 
-    pub struct CBSSet<CoT: StateCo, SymT: StateSym> {
+    struct CBSSet<CoT: StateCo, SymT: StateSym> {
         set: HashSet<FullCBS<CoT, SymT>>,
     }
 
-    pub struct PartialCBS<CoT: StateCo, SymT: StateSym> {
+    struct PartialCBS<CoT: StateCo, SymT: StateSym> {
         state_c: CoT,
         state_s: Conj<SymT>,
     }
 
-    pub enum IntermediateCBS<CoT: StateCo, SymT: StateSym> {
+    enum IntermediateCBS<CoT: StateCo, SymT: StateSym> {
         CBSMap(HashMap<BlockId, PartialCBS<CoT, SymT>>),
         PureCBSMap(PureCBS<CoT>),
     }
 
-    pub struct PureCBS<CoT: StateCo> {
+    struct PureCBS<CoT: StateCo> {
         state_c: CoT,
         block: BlockId,
     }
 
-    pub enum Tree<LeafT, NodeT> {
+    enum Tree<LeafT, NodeT> {
         Branch {
             value: NodeT,
             l: Box<Tree<LeafT, NodeT>>,
@@ -85,7 +122,7 @@ mod data {
         },
     }
 
-    pub struct CBSTree<CoT: StateCo, SymT: StateSym> {
+    struct CBSTree<CoT: StateCo, SymT: StateSym> {
         tree: Tree<PureCBS<CoT>, SymT>,
     }
 }
